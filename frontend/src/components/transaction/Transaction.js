f7daabdf399f5c10593bfa9923c07bbe2091917b
bobama123 <bobbychaudhry@hotmail.co.uk>
2025-01-31 13:18:02 +0000

    cleaning up the file

--- frontend/src/components/transaction/Transaction.js
+++ frontend/src/components/transaction/Transaction.js
@@ -1,27 +1,6 @@
-// import React from 'react';
-
-// const Transaction = ({ transaction, onDelete }) => {
-//   return (
-//     <div className="transaction">
-//       <p>Amount: {transaction.amount}</p>
-//       <p>Date: {transaction.date}</p>
-//       <p>Category: {transaction.category}</p>
-//       <p>Description: {transaction.description}</p>
-//       <button onClick={() => onDelete(transaction._id)}>Delete</button>
-//     </div>
-//   );
-// };
-
-// export default Transaction;
-
-
 import React from "react";
 
 const Transaction = ({ transaction, onDelete }) => {
-  if (!transaction || !transaction._id) {
-    console.error("Transaction ID is missing:", transaction);
-    return null;
-  }
 
   return (
     <div className="transaction">
